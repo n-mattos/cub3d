eb5cc27e6149f3369ee9d086212cd4d5f9f6039c
Noah Mattos Oudejans <noahmattosoudejans@icloud.com>
2025-06-23 15:28:22 +0200

    Changed EMPTY from 'x' to ' '

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,17 +6,22 @@
 /*   By: nmattos- <nmattos-@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 12:53:01 by nmattos-          #+#    #+#             */
-/*   Updated: 2025/06/23 13:48:33 by nmattos-         ###   ########.fr       */
+/*   Updated: 2025/06/23 15:27:38 by nmattos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	CUB3D_H
 # define CUB3D_H
 
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "../libft/libft.h"
 // include mlx library
 
 typedef enum e_tile {
-	EMPTY = 'x',
+	EMPTY = ' ',
 	WALL = '1',
 	FLOOR = '0',
 	NORTH = 'N',
