7563a20f4a5d5dc19d1685f76440054d263b9800
Noah Mattos Oudejans <noahmattosoudejans@icloud.com>
2025-06-24 13:55:26 +0200

    Split functions to fit 25 lines

--- src/parsing/parse_map.c
+++ src/parsing/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: nmattos- <nmattos-@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 13:14:46 by nmattos-          #+#    #+#             */
-/*   Updated: 2025/06/24 12:09:35 by nmattos-         ###   ########.fr       */
+/*   Updated: 2025/06/24 13:54:51 by nmattos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,45 @@ char	*get_raw_map_data(int fd)
 	return(raw_map);
 }
 
+void	clean_map(int **map, size_t i)
+{
+	while (i >= 0)
+		free(map[i--]);
+	free(map);
+}
+
+int	get_tile(char c)
+{
+	if (is_whitespace(c))
+		return (EMPTY);
+	return (c);
+}
+
+int	**create_map(t_level *level, char *raw_map)
+{
+	size_t	i;
+	size_t	x;
+	size_t	y;
+
+	i = 0;
+	y = 0;
+	while (raw_map[i] != '\0')
+	{
+		x = 0;
+		level->map[y] = malloc(sizeof(int) * (chars_till_eol(&raw_map[i])) + 1);
+		if (level->map[y] == NULL)
+			return (clean_map(level->map, y - 1), free(level), NULL);	// clean level->map[(x ∈ [0, y - 1])]
+		while (raw_map[i] != '\n' && raw_map[i] != '\0')
+			level->map[y][x++] = get_tile(raw_map[i++]);
+		level->map[y++][x] = '\0';
+		if (raw_map[i] == '\n')
+			i++;
+	}
+	free(raw_map);
+	level->map[y] = NULL;
+	return (level->map);
+}
+
 t_level	*parse_map(int fd)
 {
 	t_level	*level;
@@ -79,31 +118,9 @@ t_level	*parse_map(int fd)
 	raw_map = get_raw_map_data(fd);
 	level->map = malloc(sizeof(int *) * (count_newlines(raw_map) + 2));
 	if (level->map == NULL)
-		return (NULL);	// clean level
-	int	i = 0;
-	int	y = 0;
-	int	x = 0;
-	while (raw_map[i] != '\0')
-	{
-		level->map[y] = malloc(sizeof(int) * (chars_till_eol(&raw_map[i])) + 1);
-		if (level->map[y] == NULL)
-			return (NULL);	// clean level->map[(x ∈ [0, y - 1])]	+	clean level
-		while (raw_map[i] != '\n' && raw_map[i] != '\0')
-		{
-			if (is_whitespace(raw_map[i]))
-				level->map[y][x] = EMPTY;
-			else
-				level->map[y][x] = raw_map[i];
-			x++;
-			i++;
-		}
-		level->map[y][x] = '\0';
-		if (raw_map[i] != '\0')
-			i++;	// skip newline char
-		y++;	// continue to next line
-		x = 0;	// start at first element
-	}
-	free(raw_map);
-	level->map[y] = NULL;
+		return (free(level), NULL);
+	level->map = create_map(level, raw_map);
+	if (level->map == NULL)
+		return (NULL);
 	return (level);
 }
