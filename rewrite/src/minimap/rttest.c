5e2ad95c7f700892931dad9cad4053c77b654302
Noma <noahmattosoudejans@icloud.com>
2025-07-14 15:12:00 +0200

    cleaned up rttest.c

--- rewrite/src/minimap/rttest.c
+++ rewrite/src/minimap/rttest.c
@@ -6,7 +6,7 @@
 /*   By: nmattos- <nmattos-@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/24 12:58:15 by mschippe      #+#    #+#                 */
-/*   Updated: 2025/07/14 14:32:48 by nmattos       ########   odam.nl         */
+/*   Updated: 2025/07/14 15:11:40 by nmattos       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,89 +57,145 @@ void	drawline(mlx_image_t *img, t_point a, t_point b, uint32_t color)
 	drawline_draw(img, a, b, color, pixels);
 }
 
+t_vect	calculate_delta(t_vect raydir)
+{
+	t_vect	delta;
+
+	if (fabs(raydir.x) < EPSILON)
+		delta.x = 0;
+	else
+		delta.x = fabs(1 / raydir.x);
+	if (fabs(raydir.y) < EPSILON)
+		delta.y = 0;
+	else
+		delta.y = fabs(1 / raydir.y);
+	return (delta);
+}
+
+t_vect	calculate_side(t_playerdata p, t_vect raydir, t_point map, t_vect delta, t_point *step)
+{
+	t_vect	side;
+
+	if (raydir.x < 0)
+	{
+		step->x = -1;
+		side.x = ((p.x) - map.x) * delta.x;
+	}
+	else
+	{
+		step->x = 1;
+		side.x = (map.x + 1.0 - (p.x)) * delta.x;
+	}
+	if (raydir.y < 0)
+	{
+		step->y = -1;
+		side.y = ((p.y) - map.y) * delta.y;
+	}
+	else
+	{
+		step->y = 1;
+		side.y = (map.y + 1.0 - (p.y)) * delta.y;
+	}
+	return side;
+}
+
+t_vect	calculate_raydir(mlx_image_t *img, t_playerdata p, int x)
+{
+	double	camera_x;
+	t_vect	raydir;
+
+	camera_x = 2.0 * x / (double)img->width - 1.0;
+
+	raydir.x = p.dir_x + p.plane_x * camera_x;
+	raydir.y = p.dir_y + p.plane_y * camera_x;
+	return (raydir);
+}
+
+t_point	calculate_map(t_playerdata p)
+{
+	t_point	map;
+
+	map.x = (int)p.x;
+	map.y = (int)p.y;
+	return (map);
+}
+
+void	calculate_ray(t_point *map, t_vect *side, t_vect delta, t_point step, int *hit_side, t_level *lvl)
+{
+	bool	hit;
+
+	hit = false;
+	while (!hit)
+	{
+		if (side->x < side->y)
+		{
+			side->x += delta.x;
+			map->x += step.x;
+			hit = lvl->map[map->y][map->x] == '1';
+			*hit_side = 0;
+		}
+		else
+		{
+			side->y += delta.y;
+			map->y += step.y;
+			hit = lvl->map[map->y][map->x] == '1';
+			*hit_side = 1;
+		}
+	}
+}
+
+double calculate_perpendicular_distance(t_playerdata p, t_vect raydir, t_point map, int hit_side, t_point step)
+{
+	double	perp_wall_dist;
+
+	if (hit_side == 0)
+		perp_wall_dist = (map.x - p.x + (1 - step.x) / 2) / raydir.x;
+	else
+		perp_wall_dist = (map.y - p.y + (1 - step.y) / 2) / raydir.y;
+	return (perp_wall_dist);
+}
+
+t_vect calculate_intersection(t_playerdata p, t_vect raydir, double perp_wall_dist)
+{
+	t_vect	intersect;
+
+	intersect.x = p.x + raydir.x * perp_wall_dist;
+	intersect.y = p.y + raydir.y * perp_wall_dist;
+	return (intersect);
+}
+
 void	raycast_dda(t_level *lvl, mlx_image_t *img)
 {
-	t_playerdata p = *lvl->player;
+	t_playerdata	p;
+	t_vect			side;
+	t_point 		step;
+	t_point			map;
+	t_vect			raydir;
+	t_vect			delta;
+	int				x;
+	t_vect			intersect;
+	double			perp_wall_dist;
+	int				hit_side;
 
-	int x = 0;
+	p = *lvl->player;
+	x = 0;
 	while (x < (int)img->width)
 	{
-		double camera_x = 2.0 * x / (double)img->width - 1.0;
-		double raydir_x = p.dir_x + p.plane_x * camera_x;
-		double raydir_y = p.dir_y + p.plane_y * camera_x;
+		raydir = calculate_raydir(img, p, x);
+		delta = calculate_delta(raydir);
+		map = calculate_map(p);
+		side = calculate_side(p, raydir, map, delta, &step);
 
-		double delta_x;
-		double delta_y;
-		if (fabs(raydir_x) < EPSILON)
-			delta_x = 0;
-		else
-			delta_x = fabs(1 / raydir_x);
-		if (fabs(raydir_y) < EPSILON)
-			delta_y = 0;
-		else
-			delta_y = fabs(1 / raydir_y);
+		hit_side = -1; // -1 = no hit, 0 = hit vertical wall, 1 = hit horizontal wall
+		calculate_ray(&map, &side, delta, step, &hit_side, lvl);
+		(void)hit_side;	// unused variable
 
-		int map_x = p.x;
-		int map_y = p.y;
-		double side_x;
-		double side_y;
-		t_point step;
-		if (raydir_x < 0)
-		{
-			step.x = -1;
-			side_x = ((p.x) - map_x) * delta_x;
-		}
-		else
-		{
-			step.x = 1;
-			side_x = (map_x + 1.0 - (p.x)) * delta_x;
-		}
-		if (raydir_y < 0)
-		{
-			step.y = -1;
-			side_y = ((p.y) - map_y) * delta_y;
-		}
-		else
-		{
-			step.y = 1;
-			side_y = (map_y + 1.0 - (p.y)) * delta_y;
-		}
+		perp_wall_dist = calculate_perpendicular_distance(p, raydir, map, hit_side, step);
+		(void)perp_wall_dist; // unused variable
 
-		bool hit = false;
-		int side = -1; // -1 = no hit, 0 = hit vertical wall, 1 = hit horizontal wall
-		while (!hit)
-		{
-			if (side_x < side_y)
-			{
-				side_x += delta_x;
-				map_x += step.x;
-				hit = lvl->map[map_y][map_x] == '1';
-				side = 0;
-			}
-			else
-			{
-				side_y += delta_y;
-				map_y += step.y;
-				hit = lvl->map[map_y][map_x] == '1';
-				side = 1;
-			}
-		}
-		(void)side;
+		intersect = calculate_intersection(p, raydir, perp_wall_dist);
 
-		// drawrectangle(img, (t_point){TILE_SIZE, TILE_SIZE}, (t_point){map_x * TILE_SIZE, map_y * TILE_SIZE}, 0xFFFFFFFF);
-
-		// calculate the perpendicular distance to the wall
-		double perp_wall_dist;
-		if (side == 0)
-			perp_wall_dist = (map_x - p.x + (1 - step.x) / 2) / raydir_x;
-		else
-			perp_wall_dist = (map_y - p.y + (1 - step.y) / 2) / raydir_y;
-
-		// Calculate intersection point
-		double hit_x = p.x + raydir_x * perp_wall_dist;
-		double hit_y = p.y + raydir_y * perp_wall_dist;
-
-		drawline(img, (t_point){p.x * PIXEL_SIZE, p.y * PIXEL_SIZE}, (t_point){hit_x * PIXEL_SIZE, hit_y * PIXEL_SIZE}, 0xFFd6ffcf);
+		drawline(img, (t_point){p.x * PIXEL_SIZE, p.y * PIXEL_SIZE}, (t_point){intersect.x * PIXEL_SIZE, intersect.y * PIXEL_SIZE}, 0xFFd6ffcf);
 
 		x += (int)img->width / TOTAL_RAYS;
 	}
